e556e990c36ba5b0185c5a4afe953a0e55a61b83
hanifsyabani <mh070940@gmail.com>
2024-04-14 20:33:05 +0700

    feat: add prisma

--- src/Components/Card/AddTaskCard.tsx
+++ src/Components/Card/AddTaskCard.tsx
@@ -5,7 +5,7 @@ export default function Card() {
     <div className="w-72  p-3 rounded-lg border border-gray-500 h-56 flex justify-center items-center cursor-pointer">
       <div className="flex justify-center items-center gap-3">
         <FaPlus size={20} className="text-gray-400" />
-        <h1 className="text-gray-400 text-lg  ">Add Task</h1>
+        <h1 className="text-gray-400 text-lg">Add Task New</h1>
       </div>
     </div>
   );
--- src/Components/Sidebar/SideItem.ts
+++ src/Components/Sidebar/SideItem.ts
@@ -1,5 +1,3 @@
-
-
 export interface SideItem {
   name: string;
   path: string;
